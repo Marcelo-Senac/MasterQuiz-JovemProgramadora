8da1e225a7df494c05f25b3a7eeb87f193782237
mmortiz <marcelomaortiz@hotmail.com>
2024-11-28 03:45:18 -0300

    Variaveis de configuração com banco Azure feito

---
+++ GptQuiz/Models/DB/queries/perguntas.sql
@@ -0,0 +1,371 @@
+SELECT * FROM categorias;
+
+insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o nome do monstro que enfrenta Godzilla no final de "Godzilla II: Rei dos Monstros"?',
+    'Mothra',
+    'King Ghidorah',
+    'Rodan',
+    'Mechagodzilla',
+    'King Ghidorah',
+    '',
+    2);
+    
+insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quantos filmes da série Harry Potter foram lançados?',
+    '6',
+    '7',
+    '8',
+    '9',
+    '8',
+    '',
+    2);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quem foi o primeiro imperador do Brasil?',
+    'Dom Pedro I',
+    'Dom João VI',
+    'Getúlio Vargas',
+    'Juscelino Kubitschek',
+    'Juscelino Kubitschek',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Em que ano foi realizado o primeiro Encontro Nacional de Mulheres Negras, que teve grande importância para a luta das mulheres negras no Brasil?',
+    '1990',
+    '1993',
+    '1988',
+    '2000',
+    '1993',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quem é conhecido como "O Rei do Futebol"?',
+    'Pelé',
+    'Diego Maradona',
+    'Ronaldo Fenômeno',
+    'Zico',
+    'Pelé',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o nome da primeira missão tripulada que foi enviada para a Lua?',
+    'Apollo 11',
+    'Gemini 8',
+    'Soyuz 1',
+    'Apollo 13',
+    'Apollo 11',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o maior oceano do mundo?',
+    'Oceano Atlântico',
+    'Oceano Índico',
+    'Oceano Ártico',
+    'Oceano Pacífico',
+    'Oceano Pacífico',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o maior bioma do Brasil?',
+    'Caatinga',
+    'Cerrado',
+    'Floresta Amazônica',
+    'Pampas',
+    'Floresta Amazônica',
+    '',
+    2);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quem é conhecido como o "Rei do Pop"?',
+    'Elvis Presley',
+    'Michael Jackson',
+    'Prince',
+    'David Bowie',
+    'Michael Jackson',
+    '',
+    2);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o gênero musical predominante no Carnaval do Rio de Janeiro?',
+    'Axé',
+    'Frevo',
+    'Samba',
+    'Funk',
+    'Samba',
+    '',
+    2);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é o limite de faturamento anual para o MEI em 2024?',
+    '60 mil',
+    '81 mil',
+    '149 mil',
+    '240 mil',
+    '81 mil',
+    '',
+    3);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual ferramenta administrativa é usada para planejar e monitorar as entradas e saídas financeiras da empresa?',
+    'diagrama de Gantt',
+    'plano de Negócios',
+    'fluxo de Caixa',
+    'matriz SWOT',
+    'fluxo de Caixa',
+    '',
+    3);
+    
+      insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('É exemplo de um ativo intangível?',
+    'passivo',
+    'veículos',
+    'software',
+    'bois',
+    'software',
+    '',
+    3);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual das alternativas reflete a essência do branding?',
+    'criar um logo atrativo para o banco',
+    'definir preços competitivos',
+    'construir a identidade, valores e percepções da marca pelos consumidores',
+    'divulgar produtos em canais digitais',
+    'construir a identidade, valores e percepções da marca pelos consumidores',
+    '',
+    3);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual das opções abaixo NÃO é uma função do departamento pessoal?',
+    'gerenciar a folha de pagamentos',
+    'cuidar do recrutamento e seleção de candidatos',
+    'administrar férias e benefícios',
+    'garantir o cumprimento das disposições trabalhistas',
+    'cuidar do recrutamento e seleção de candidatos',
+    '',
+    3);
+    
+      
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual auxiliar do simple present é o correto para iniciar a frase? _____ you see your friends?',
+    'do',
+    'what',
+    'are',
+    'does',
+    'do',
+    '',
+    4);
+    
+      
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual das seguintes frases está relacionada ao uso do verbo "to be"?',
+    'she are my friend',
+    'he is my brother',
+    'they am happy',
+    'i are a teacher',
+    'he is my brother',
+    '',
+    4);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quais das alternativas abaixo estão fazendo o uso correto do simple present?',
+    'mike and Ana goes at school',
+    'she love candy',
+    'he play volleyball',
+    'he watches old movies',
+    'he watches old movies',
+    '',
+    4);
+    
+      insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é a forma correta do verbo na frase:"She_____(to go)to the store every Saturday?',
+    'go',
+    'goes',
+    'goed',
+    'going',
+    'goes',
+    '',
+    4);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual das seguintes é a resposta mais apropriada para a pergunta "Como você está?',
+    'l am Brazilian',
+    'l am a teacher',
+    'l am 10 years old',
+    'l am fine,thanks',
+    'l am fine,thanks',
+    '',
+    4);
+    
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Sobre ovos?',
+    'devem ser deixados do lado de fora da geladeira',
+    'devem ser colocados na porta da geladeira',
+    'devem ser refrigerado na geladeira na parte interna',
+    'devem ser colocados dentro do armário',
+    'devem ser refrigerado na geladeira na parte interna',
+    '',
+    5);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Para que serve as cores das placas de corte?',
+    'enfeitar a cozinha',
+    'para evitar a contaminação cruzada entre alimentos crus e cozidos',
+    'para estimular a venda de placas de corte',
+    'oferecer design colorido à cozinha',
+    'para evitar a contaminação cruzada entre alimentos crus e cozidos',
+    '',
+    5);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual a forma correta de higienizar verduras e frutas?',
+    'só com água corrente',
+    'com solução clorada ou vinagre',
+    'com sal e vinagre',
+    'com água e detergente',
+    'com solução clorada ou vinagre',
+    '',
+    5);
+    
+    insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Como deve estar o manipulador de alimentos?',
+    'Sandália de dedo,adornos e cabelos soltos',
+    'unhas grandes,coloridos e camisa regata',
+    'sem lavar as mãos e de touca descartável',
+    'touca,avental,sapato fechado,sem adornos e mão higienizados e unhas curtas sem esmalte',
+    'touca,avental,sapato fechado,sem adornos e mão higienizados e unhas curtas sem esmalte',
+    '',
+    5);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Sobre a higiene dos utensílios?',
+    'bucha amarela com detergente',
+    'palha de aço com limpa alumínio',
+    'soda cáustica',
+    'somente água corrente',
+    'bucha amarela com detergente',
+    '',
+    5);
+    
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quais os três principais funções do TST?',
+    'montar planilha de custo,efetuar compra,instalação elétrica',
+    'líquido periciais,medidas e soluções,acompanhar processos nas diversas esferas',
+    'atendimento contábil,psicológico e profissional',
+    'julgar ações trabalhistas individuais de empregados e empregadores',
+    'líquido periciais,medidas e soluções,acompanhar processos nas diversas esferas',
+    '',
+    6);
+    
+      insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é a principal finalidade do equipamento de proteção individual(EPI)?',
+    'aumentar a produtividade do trabalhador',
+    'substituir a necessidade de medidas coletivo de segurança',
+    'proteger trabalhador onde riscos que ameaçam sua segurança  e saúde',
+    'melhorar a aparência do trabalho',
+    'proteger trabalhador onde riscos que ameaçam sua segurança  e saúde',
+    '',
+    6);
+    
+    
+      insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual é a norma regulamentadora (NR) que trata da segurança no trabalho em máquinas e equipamentos?',
+    'NR-10',
+    'NR-12',
+    'NR-15',
+    'NR-35',
+    'NR-12',
+    '',
+    6);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Qual documento é obrigatório para realização de atividades de altura conforme NR-35?',
+    'ordem de serviço',
+    'permissão de trabalho',
+    'ficha de EPI',
+    'relatório técnico de segurança',
+    'permissão de trabalho',
+    '',
+    6);
+    
+     insert into perguntas
+	(enunciado, alternativa1, alternativa2, alternativa3, alternativa4, resposta_correta, dica, fk_categoria_id)
+values
+	('Quantos NR temos vigente?',
+    '51 e 2 revogada',
+    '12 revogada, 21 atualizada, 3 excluída',
+    '38 NRs e 02 revogada',
+    'o número atual de NRs em vigor é 30',
+    '38 NRs e 02 revogada',
+    '',
+    6);
+    
+    
+      
+    
+    
